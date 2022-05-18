e9178cc473da2a871df8e60f278ae5337a1ecf6a
Sergey Petrinich <petrinichshadow@gmail.com>
2022-05-18 19:13:27 +0300

    added images to constants

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,3 +1,4 @@
 import images from "./images";
+const { footerImages, headerImages, lotsImages, lotsAllImages, mainImages, newsImages, quotesImages, servicesImages } = images;
 
 export default {};
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,127 @@
-import bg from "./../assets/img/bg.jpg";
-
-const defaultImages = {
-    bg,
+import footerLogo from "./../assets/img/footer/01.png";
+import footerBack from "./../assets/img/footer/02.png";
+const footerImages = {
+    footerLogo,
+    footerBack,
 };
 
-export default { defaultImages };
+import headerLogo from "./../assets/img/header/Logo.png";
+import headerGlobe from "./../assets/img/header/02.png";
+import headerUser from "./../assets/img/header/03.png";
+import headerKey from "./../assets/img/header/04.png";
+import headerHand from "./../assets/img/header/05.png";
+import headerHammer from "./../assets/img/header/06.png";
+const headerImages = {
+    headerLogo,
+    headerGlobe,
+    headerUser,
+    headerKey,
+    headerHand,
+    headerHammer,
+};
+
+import lot01 from "./../assets/img/lots/01.jpg";
+import lot02 from "./../assets/img/lots/02.jpg";
+import lot03 from "./../assets/img/lots/03.jpg";
+import lotIconWatch from "./../assets/img/lots/icons/watch.png";
+import lotIconTime from "./../assets/img/lots/icons/time.png";
+import lotIconEye from "./../assets/img/lots/icons/eye.png";
+import lotIconHand from "./../assets/img/lots/icons/hand.png";
+import lotIconArrow from "./../assets/img/lots/icons/arrow.png";
+import lotIconCircle from "./../assets/img/lots/icons/circle.png";
+const lotsImages = {
+    lot01,
+    lot02,
+    lot03,
+    lotIconWatch,
+    lotIconTime,
+    lotIconEye,
+    lotIconHand,
+    lotIconArrow,
+    lotIconCircle,
+};
+
+import lots01 from "./../assets/img/lots-all/01.png";
+import lots02 from "./../assets/img/lots-all/02.png";
+import lots03 from "./../assets/img/lots-all/03.png";
+import lots04 from "./../assets/img/lots-all/04.png";
+import lots05 from "./../assets/img/lots-all/05.png";
+import lots06 from "./../assets/img/lots-all/06.png";
+import lots07 from "./../assets/img/lots-all/07.png";
+import lots08 from "./../assets/img/lots-all/08.png";
+import lots09 from "./../assets/img/lots-all/09.png";
+import lotsBg from "./../assets/img/lots-all/bg.png";
+import lotsBgHead from "./../assets/img/lots-all/bg_head.png";
+import lotsIcon01 from "./../assets/img/lots-all/icons/01.png";
+import lotsIcon02 from "./../assets/img/lots-all/icons/02.png";
+import lotsIcon03 from "./../assets/img/lots-all/icons/03.png";
+import lotsIcon04 from "./../assets/img/lots-all/icons/04.png";
+import lotsIcon05 from "./../assets/img/lots-all/icons/05.png";
+import lotsIcon06 from "./../assets/img/lots-all/icons/06.png";
+import lotsIcon07 from "./../assets/img/lots-all/icons/07.png";
+import lotsIcon08 from "./../assets/img/lots-all/icons/08.png";
+import lotsIcon09 from "./../assets/img/lots-all/icons/09.png";
+import lotsIconModel from "./../assets/img/lots-all/icons/model.png";
+const lotsAllImages = {
+    lots01,
+    lots02,
+    lots03,
+    lots04,
+    lots05,
+    lots06,
+    lots07,
+    lots08,
+    lots09,
+    lotsBg,
+    lotsBgHead,
+    lotsIcon01,
+    lotsIcon02,
+    lotsIcon03,
+    lotsIcon04,
+    lotsIcon05,
+    lotsIcon06,
+    lotsIcon07,
+    lotsIcon08,
+    lotsIcon09,
+    lotsIconModel,
+};
+
+import arrowMain from "./../assets/img/main/arrow.png";
+import arrowCircle from "./../assets/img/main/circle.png";
+import arrowBg from "./../assets/img/main/bg.jpg";
+const mainImages = {
+    arrowMain,
+    arrowCircle,
+    arrowBg,
+};
+
+import news01 from "./../assets/img/news/01.png";
+import news02 from "./../assets/img/news/02.png";
+import news03 from "./../assets/img/news/03.png";
+import news04 from "./../assets/img/news/04.png";
+const newsImages = {
+    news01,
+    news02,
+    news03,
+    news04,
+};
+
+import newsIcon from "./../assets/img/quotes/01.png";
+import newsAvatar from "./../assets/img/quotes/02.png";
+const quotesImages = {
+    newsIcon,
+    newsAvatar,
+};
+
+import servicesIcon from "./../assets/img/services/01.png";
+import services01 from "./../assets/img/services/02.png";
+import services02 from "./../assets/img/services/03.png";
+import services03 from "./../assets/img/services/04.png";
+const servicesImages = {
+    servicesIcon,
+    services01,
+    services02,
+    services03,
+};
+
+export default { footerImages, headerImages, lotsImages, lotsAllImages, mainImages, newsImages, quotesImages, servicesImages };
